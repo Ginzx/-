04cd2296dc800704d73c924484b16bda8f952d99
wqm <874633732@qq.com>
2018-06-21 14:53:34 +0800

    3

--- 00项目源码/flower/src/main/java/com/slsd/service/AdminService.java
+++ 00项目源码/flower/src/main/java/com/slsd/service/AdminService.java
@@ -2,6 +2,13 @@ package com.slsd.service;
 
 import com.slsd.entity.Admin;
 
+/**
+* @ClassName: AdminService
+* @Description:TODO(注册方法注入)
+* @author: 王启明
+* @date: 2018年6月21日 下午2:52:42
+*
+*/
 public interface AdminService {
 
 	public int login(Admin admin);
--- 00项目源码/flower/src/main/java/com/slsd/service/impl/AdminServiceImpl.java
+++ 00项目源码/flower/src/main/java/com/slsd/service/impl/AdminServiceImpl.java
@@ -6,6 +6,13 @@ import com.slsd.dao.Admindao;
 import com.slsd.entity.Admin;
 import com.slsd.service.AdminService;
 
+/**
+* @ClassName: AdminServiceImpl
+* @Description:TODO(管理员功能实现)
+* @author: Administrator
+* @date: 2018年6月21日 下午2:53:06
+*
+*/
 @Service("AdminService")
 public class AdminServiceImpl implements AdminService {
 
