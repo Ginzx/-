5afcd019c61c427e04b15e4bd75ac3d4d52035ba
wqm <874633732@qq.com>
2018-06-25 10:00:21 +0800

    111

--- 00项目源码/flower/src/main/java/com/slsd/controller/LoginController.java
+++ 00项目源码/flower/src/main/java/com/slsd/controller/LoginController.java
@@ -1,5 +1,6 @@
 package com.slsd.controller;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
@@ -7,15 +8,26 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.slsd.entity.Admin;
+import com.slsd.service.AdminService;
+
 @Controller
 @RequestMapping("/login")
 public class LoginController {
+	
+	@Resource
+	private AdminService adminService;
 
-	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String login(HttpServletRequest request, Model model) {
 		String name=request.getParameter("name");
 		String pwd=request.getParameter("pwd");
-		return null;
+		Admin admin=new Admin();
+		admin.setName(name);
+		admin.setPwd(pwd);
+		int num = this.adminService.login(admin);
+		System.out.println(num);
+		return "index";
 		
 	}
 }
