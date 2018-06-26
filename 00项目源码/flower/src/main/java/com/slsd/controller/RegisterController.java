0ea12ad38d98a6b68478abca2b0ad749eaa30035
wqm <874633732@qq.com>
2018-06-26 11:29:58 +0800

    123

--- 00项目源码/flower/src/main/java/com/slsd/controller/RegisterController.java
+++ 00项目源码/flower/src/main/java/com/slsd/controller/RegisterController.java
@@ -1,14 +1,21 @@
 package com.slsd.controller;
 
-import java.io.File;  
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.List;  
 import java.util.UUID;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;  
-import org.springframework.stereotype.Controller;  
-import org.springframework.web.bind.annotation.RequestMapping;  
-import org.springframework.web.multipart.MultipartFile;  
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
 import com.slsd.entity.User;
 import com.slsd.service.UserService;
 
@@ -17,6 +24,11 @@ public class RegisterController {
 
 	@Resource
 	private UserService userService;
+	
+	@RequestMapping(value = "/registerin", method = RequestMethod.GET)
+	public String loginin() {
+		return "register";
+	}
 
 	@RequestMapping(value = "/register")
 	public String addUser(HttpServletRequest request, User user, MultipartFile pictureFile) throws Exception {
@@ -27,8 +39,16 @@ public class RegisterController {
         String username=request.getParameter("username");
         String pwd=request.getParameter("pwd");
         String pwd1=request.getParameter("pwd1");
-        String address=request.getParameter("address");
+        String phone=request.getParameter("phone");
         
-		return null;
+        user.setPhone(phone);
+        user.setUsername(username);
+        user.setUserpwd(pwd);
+        
+        boolean flag=userService.addUser(user);
+        System.out.println(flag);
+		return "register";
 	}
+	
+	
 }
