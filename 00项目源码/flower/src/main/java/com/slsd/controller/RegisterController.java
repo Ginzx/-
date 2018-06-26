0a8aa9a017f4635fd028c9735ea1ff5da244089b
wqm <874633732@qq.com>
2018-06-26 13:51:13 +0800

    333

--- 00项目源码/flower/src/main/java/com/slsd/controller/RegisterController.java
+++ 00项目源码/flower/src/main/java/com/slsd/controller/RegisterController.java
@@ -3,7 +3,7 @@ package com.slsd.controller;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;  
+import java.util.List;
 import java.util.UUID;
 
 import javax.annotation.Resource;
@@ -24,31 +24,30 @@ public class RegisterController {
 
 	@Resource
 	private UserService userService;
-	
+
 	@RequestMapping(value = "/registerin", method = RequestMethod.GET)
 	public String loginin() {
 		return "register";
 	}
 
-	@RequestMapping(value = "/register")
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
 	public String addUser(HttpServletRequest request, User user, MultipartFile pictureFile) throws Exception {
 
-        //把初始图片存储路径保存到数据库  
-        user.setPhoto("upload/touxiang/0.jpg");
-        
-        String username=request.getParameter("username");
-        String pwd=request.getParameter("pwd");
-        String pwd1=request.getParameter("pwd1");
-        String phone=request.getParameter("phone");
-        
-        user.setPhone(phone);
-        user.setUsername(username);
-        user.setUserpwd(pwd);
-        
-        boolean flag=userService.addUser(user);
-        System.out.println(flag);
+		// 把初始图片存储路径保存到数据库
+		user.setPhoto("upload/touxiang/0.jpg");
+
+		String username = request.getParameter("username");
+		String pwd = request.getParameter("pwd");
+		String pwd1 = request.getParameter("pwd1");
+		String phone = request.getParameter("phone");
+
+		user.setPhone(phone);
+		user.setUsername(username);
+		user.setUserpwd(pwd);
+
+		boolean flag = userService.addUser(user);
+		System.out.println(flag);
 		return "register";
 	}
-	
-	
+
 }
